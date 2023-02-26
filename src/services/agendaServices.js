6cf071c1f5b1ade7a21de24c8cecb0b6143e7ded
dannlion <117786966+dannlion@users.noreply.github.com>
2023-02-26 18:43:11 -0300

    Update agendaServices.js
    EDIT: URL API HTTP > HTTPS

--- src/services/agendaServices.js
+++ src/services/agendaServices.js
@@ -2,7 +2,7 @@ import useApi from "@/composables/useApi";
 
 export default function agendaService() {
   const { list, getById, post, update, excluir } = useApi(
-    "http://ampere1.zapto.org:3000"
+    "https://ampere1.zapto.org:3000"
   );
 
   return {
